1f63f886bd6520a71c2c288867b787f973798bc9
gadziunet <gadziunet@gmail.com>
2026-04-01 00:19:00 +0200

    Remove Docker status check from installation script
    Remove status check for Docker service after installation.

--- docker_install.sh
+++ docker_install.sh
@@ -27,7 +27,6 @@ systemctl start docker
 systemctl enable docker
 usermod -aG docker ${USER}
 systemctl restart docker
-systemctl status docker
 
 # Docker Compose Install
 curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
