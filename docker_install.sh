a3427b0f815205dc5207cd5f67692526d73ce0b0
gadziunet <gadziunet@gmail.com>
2026-04-01 00:02:57 +0200

    Add Docker and Docker Compose installation script
    This script installs Docker and Docker Compose on an Ubuntu system, including necessary dependencies and configurations.

---
+++ docker_install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+sudo apt update -y
+sudo apt-get upgrade -y 
+
+# Docker Install
+
+sudo apt-get update -y
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+apt-get update -y
+
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ${USER}
+systemctl restart docker
+systemctl status docker
+
+# Docker Compose Install
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+apt update -y
+apt upgrade -y
